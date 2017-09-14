a6979a63a84de495ed33a2ff8d423cfbaa377d8e
Henrique de Souza Sumitomo <henrique.sumitomo@linx.com.br>
2017-09-14 18:34:05 -0300

    Retorna o dia, dia da semana e qual mês foi selecionado

--- src/pages/calendar-page/calendar-page.ts
+++ src/pages/calendar-page/calendar-page.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage,
          NavController, 
          NavParams,  
-         ModalController, 
-         AlertController} from 'ionic-angular';
+         ModalController} from 'ionic-angular';
 import * as moment from 'moment';
-//import { CalendarPopover } from '../calendar-popover/calendar-popover';
 
 @IonicPage()
 @Component({
@@ -25,29 +23,18 @@ export class CalendarPage {
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams, 
-    private modalCtrl: ModalController, 
-    private alertCtrl: AlertController) {
+    private modalCtrl: ModalController) {
   }
  
   onViewTitleChanged(title) {
     this.viewTitle = title;
   }
  
-  onEventSelected(event) {
-    let start = moment(event.startTime).format('LLLL');
-    let end = moment(event.endTime).format('LLLL');
-    
-    let alert = this.alertCtrl.create({
-      title: '' + event.title,
-      subTitle: 'From: ' + start + '<br>To: ' + end,
-      buttons: ['OK']
-    })
-    alert.present();
-  }
- 
   onTimeSelected(ev) {
     this.selectedDay = ev.selectedTime;
-    console.log(this.calendar);
+    console.log("Dia: " + this.selectedDay.getDate());
+    console.log("Dia da Semana: " + this.selectedDay.getDay()); //Vai de 0 a 6
+    console.log("Mês: " + this.selectedDay.getMonth());    
   }
   
   showOptions($event){
