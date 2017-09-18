669e56b47d43f98a98476d5d19d98a8c2102e008
Henrique de Souza Sumitomo <henrique.sumitomo@linx.com.br>
2017-09-18 15:58:06 -0300

    Adicionado a função para verificar se o ano é bissexto

--- src/pages/calendar-page/calendar-page.ts
+++ src/pages/calendar-page/calendar-page.ts
@@ -26,6 +26,21 @@ export class CalendarPage {
     {"Day": -1, "DWeek": 6, "Month": -1}
   ];
 
+  selectedMonth = [
+    {"Name": "Janeiro", "Days": 31},
+    {"Name": "Fevereiro", "Days": 28},
+    {"Name": "Março", "Days": 31},
+    {"Name": "Abril", "Days": 30},
+    {"Name": "Maio", "Days": 31},
+    {"Name": "Junho", "Days": 30},
+    {"Name": "Julho", "Days": 31},
+    {"Name": "Agosto", "Days": 31},
+    {"Name": "Setembro", "Days": 30},
+    {"Name": "Outubro", "Days": 31},
+    {"Name": "Novembro", "Days": 30},
+    {"Name": "Dezembro", "Days": 31},
+  ];
+
   calendar = {
     mode: 'month',
     currentDate: new Date()
@@ -66,12 +81,27 @@ export class CalendarPage {
   changeSelectedWeek(day, wday, month){
     for (var _i = 0; _i < this.selectedWeek.length; _i++) {
       this.selectedWeek[_i].Day = day - wday + _i;
-      this.selectedWeek[_i].Month = month;
+      if(this.selectedWeek[_i].Day < 1){
+        this.selectedWeek[_i].Month = month-1;
+        this.isAnoBissexto(this.selectedWeek[_i].Month);
+        this.selectedWeek[_i].Day = this.selectedMonth[this.selectedWeek[_i].Month].Days - wday + _i;        
+      }
     }
     console.log(this.selectedWeek);    
   }
 
   selectDay(){
     this.calendar.mode = "day";
+  }
+
+  isAnoBissexto(month){
+    if(month === 1){
+      if(this.selectedDay.getFullYear()%4 === 0){
+        this.selectedMonth[1].Days = 29;
+      }
+      else{
+        this.selectedMonth[1].Days = 28;
+      }
+    }
   }
 }
