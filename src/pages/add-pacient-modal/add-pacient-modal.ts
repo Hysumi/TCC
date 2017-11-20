4980e40a37c8e052e902c6b26a2d72d1ce6bb994
Henrique de Souza Sumitomo <henrique.sumitomo@linx.com.br>
2017-11-20 19:54:07 -0200

    feita página de adicionar paciente

--- src/pages/add-pacient-modal/add-pacient-modal.ts
+++ src/pages/add-pacient-modal/add-pacient-modal.ts
@@ -55,9 +55,36 @@ export class AddPacientModalPage {
       this.user.address = "Sem Endereço";
     }
     if(!this.user.alergia){
-      this.user.address = "Sem Alergia";
+      this.user.alergia = "Sem Alergia";
+    }
+    
+    if(!this.user.date){
+      this.user.date = new Date();
     }
 
+    if(!this.user.bloodType){
+      this.user.bloodType = -1;
+    }
+
+    if(!this.user.email){
+      this.user.email = "Sem Email";
+    }
+
+    if(!this.user.name){
+      this.user.name = "Sem Nome";
+    }
+
+    if(!this.user.phone1){
+      this.user.phone1 = "Sem Telefone Principal";
+    }
+
+    if(!this.user.phone2){
+      this.user.phone2 = "Sem Telefone Secundário";
+    }
+
+    if(!this.user.rg){
+      this.user.rg = "Sem RG";
+    }
   }
   
 }
