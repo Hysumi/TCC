13effaa0dfd9cb96710963b7d025b2b632a19a4b
Henrique Sumitomo <hysumi@gmail.com>
2017-11-21 12:56:20 -0200

    Pegando dados do banco de dados

---
+++ src/models/consulta/consulta.ts
@@ -0,0 +1,8 @@
+export interface Consulta{
+    name: string;
+    phone: string;
+    initialDate: string;
+    endDate: string;
+    type: string;
+    obs: string;
+}
--- src/pages/calendar-page/calendar-page.ts
+++ src/pages/calendar-page/calendar-page.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { IonicPage,
-         NavController, 
-         NavParams, 
-         ModalController} from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ModalController
+} from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { ConsultaCalendar } from '../../models/consulta/consulta-calendar';
+import { Consulta } from '../../models/consulta/consulta';
 
 @IonicPage()
 @Component({
   selector: 'page-calendar-page',
   templateUrl: 'calendar-page.html',
 })
+
 export class CalendarPage {
   options: string = "day";
   eventSource = [];
@@ -17,41 +23,49 @@ export class CalendarPage {
   selectedDay = new Date();
   selectedDate;
 
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams,
-    private modal: ModalController) {
-  }
+  consultaList = []
+  consultaCalendarList = {} as ConsultaCalendar[];
 
-  months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", 
-            "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+  months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
+    "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
   calendar = {
     mode: 'day',
     currentDate: new Date()
   };
 
-  public selectOption(){
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private modal: ModalController,
+    private database: AngularFireDatabase) {
+
+    this.database.list<Consulta>('consulta-list').valueChanges().subscribe(_data =>{
+      this.consultaList = _data;
+    });    
+  }
+
+  public selectOption() {
     this.calendar.mode = this.options;
   }
 
-  onViewTitleChanged(e){
-      this.viewTitle = e;
+  onViewTitleChanged(e) {
+    this.viewTitle = e;
   }
 
   onTimeSelected(ev) {
     console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
-        (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
-    
-    if(this.selectedDate){
-      if(this.calendar.mode == 'month'){
-        if(this.selectedDate == ev.selectedTime){        
+      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+
+    if (this.selectedDate) {
+      if (this.calendar.mode == 'month') {
+        if (this.selectedDate == ev.selectedTime) {
           this.options = 'day';
           this.calendar.mode = this.options;
         }
       }
-      else{
+      else {
         this.selectedDate = ev.selectedTime;
         //Caso tenha eventos, abrir o evento
-        if(ev.events !== undefined && ev.events.length !== 0){
+        if (ev.events !== undefined && ev.events.length !== 0) {
 
         }
         else { //Caso não tenha, abrir para cadastrar uma nova consulta
@@ -61,7 +75,7 @@ export class CalendarPage {
         }
       }
     }
-    else{
+    else {
       this.selectedDate = ev.selectedTime;
     }
   }
@@ -70,7 +84,7 @@ export class CalendarPage {
     console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
   }
 
-  criarConsulta(time){
+  criarConsulta(time) {
     const criarConsulta = this.modal.create('ConsultaModalPage', time);
     criarConsulta.present();
   }
