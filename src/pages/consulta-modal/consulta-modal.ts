945aba7bbf71353fe39c76e1ae4522e65061379f
Henrique de Souza Sumitomo <henrique.sumitomo@linx.com.br>
2017-11-21 22:39:20 -0200

    Começando ajustes para update

--- src/pages/consulta-modal/consulta-modal.ts
+++ src/pages/consulta-modal/consulta-modal.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-import { Consulta } from '../../models/consulta/consulta';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
@@ -11,7 +10,7 @@ import { Observable } from 'rxjs/Observable';
 })
 export class ConsultaModalPage {
 
-  consulta = {} as Consulta;
+  consulta;
   consultaRef$: Observable<any[]>;
 
   constructor(public navCtrl: NavController,
@@ -20,10 +19,45 @@ export class ConsultaModalPage {
     private database: AngularFireDatabase) {
 
     this.consultaRef$ = this.database.list('consulta-list').valueChanges();
+    this.consulta = this.navParams.data;
+    console.log(this.consulta);
   }
   
   closeConsultaPage() {
     this.view.dismiss();
   }
 
+  atualizarConsulta(){
+    this.checkIfNull();
+    this.closeConsultaPage();
+  }
+
+  buscarPerfil(){
+    this.closeConsultaPage();    
+  }
+
+  checkIfNull() {
+    if (!this.consulta.name) {
+      this.consulta.name = "Sem Nome";
+    }
+    if (!this.consulta.phone) {
+      this.consulta.phone = "Sem Telefone";
+    }
+
+    if (!this.consulta.initialDate) {
+      this.consulta.initialDate = new Date().toISOString().slice(0, 16);
+    }
+
+    if (!this.consulta.endDate) {
+      this.consulta.endDate = new Date().toISOString().slice(0, 16);
+    }
+
+    if (!this.consulta.type) {
+      this.consulta.type = "Sem Tipo";
+    }
+
+    if (!this.consulta.obs) {
+      this.consulta.obs = "Sem Observações";
+    }
+  }
 }
